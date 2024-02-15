f58d5722764babd167fe7a8273c29b06e01d2051
Jad229 <jad229@njit.edu>
2024-02-15 18:35:03 -0500

    fixed navbar import statement

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import { AuthLinks } from "./AuthLinks";
 
 export const Navbar = () => {
   return (
-    <nav className=" w-full sticky top-0 left-0 right-0 flex justify-between items-center z-10">
+    <nav className="w-full sticky top-0 left-0 right-0 flex justify-between items-center z-10">
       <div>
         <Link href="/">
           <Image src="/logo.jpg" alt="tribe podcast" width={100} height={100} />
