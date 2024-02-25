1d402af3e6b3fc39a58dcc5fd7272656c8344b5f
kadeem-lewis <email@kadeemlewis.com>
2024-02-24 23:53:12 -0500

    featured component now gets data from prop passed from parent

--- components/featured/Featured.tsx
+++ components/featured/Featured.tsx
@@ -3,35 +3,15 @@ import { Button } from "../ui/button";
 import { buttonVariants } from "@/components/ui/button";
 import { format } from "date-fns";
 
-async function getAllEpisodes() {
-  const accessTokenRequest = await fetch(
-    `https://accounts.spotify.com/api/token`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: `grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`,
-    },
-  );
-  const accessToken = await accessTokenRequest.json();
-  const req = await fetch(
-    "https://api.spotify.com/v1/shows/3FVxSoGAovaqVF2ZwTKZyE/episodes?offset=0&limit=50",
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken.access_token}`,
-      },
-    },
-  );
-
-  const data = await req.json();
-  return data;
-}
+type FeaturedProps = {
+  featuredPodcast: {
+    name: string;
+    release_date: string;
+  };
+};
 
 //TODO: fix link to episodes
-export const Featured = async () => {
-  const data = await getAllEpisodes();
-
+export const Featured = async ({ featuredPodcast }: FeaturedProps) => {
   return (
     <section className="my-10 flex flex-col gap-5">
       <div className="flex flex-wrap items-center justify-center gap-2 p-4">
@@ -55,9 +35,9 @@ export const Featured = async () => {
       <div className="relative h-80 rounded-t-3xl bg-black bg-opacity-30 bg-microphone6 bg-cover bg-center md:mx-auto md:w-5/6 xl:w-full">
         <div className="absolute bottom-0 left-0 right-0 flex h-1/3 flex-col items-center justify-center bg-white bg-opacity-10 backdrop-blur-sm">
           <h2 className=" text-center text-xl font-bold tracking-wide text-white">
-            {data.items[0].name}
+            {featuredPodcast.name}
           </h2>
-          <p>{format(data.items[0].release_date, "eeee MMM do, yyyy")}</p>
+          <p>{format(featuredPodcast.release_date, "eeee MMM do, yyyy")}</p>
         </div>
       </div>
     </section>
